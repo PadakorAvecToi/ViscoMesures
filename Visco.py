dcd063f92d75ededaa895a531e3c3156288a4afc
Nayati-Matrat <nayati.matrat@gmail.com>
2023-02-15 17:43:42 +0100

    Update Visco.py

--- Visco.py
+++ Visco.py
@@ -12,18 +12,17 @@ window.config(background="#304562")
 
 #Création d'une image
 
-width = 300
-height = 300
-image = PhotoImage(file="logouppa.png")
+width = 2560
+height = 966
+image = PhotoImage(file="logouppa.png").subsample(5)
 canvas = Canvas(window, width=width, height=height, bg='#304562', bd=0, highlightthickness=0)
-canvas.create_image(width/2, height/2, image=image)
+canvas.create_image(width/60, height/30, image=image)
 canvas.pack(anchor='nw')
 
 #Création d'un boutton
-Premier_boutton = Button(text="test du boutton", font=("Courrier", 15), fg='#B2BE12', bg='#233448')
-Premier_boutton.pack(anchor='nw')
-
-
+Premier_boutton = Button(text="test du boutton", font=("Courrier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0)
+Premier_boutton.pack()
+Premier_boutton.place(x=10, y=100)
 
 #Afficher la fenètre
 window.mainloop()
