b3861af11d85ab2544ea72cbfc0737dd0e8e4063
Nayati-Matrat <nayati.matrat@gmail.com>
2023-02-15 16:14:19 +0100

    Create Visco.py

---
+++ Visco.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+#création de la fenètre
+window = Tk()
+
+#Modifications de la fenètre
+window.title("Banc de Viscosité")
+window.geometry("1080x720")
+window.minsize(1080,720)
+window.iconbitmap("uppa.ico")
+
+#Afficher la fenètre
+window.mainloop()
