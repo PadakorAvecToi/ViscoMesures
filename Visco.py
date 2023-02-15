6b2f236e4c290724fa74c194f815c4f0fccc5781
Nayati-Matrat <nayati.matrat@gmail.com>
2023-02-15 16:57:31 +0100

    Update Visco.py

--- Visco.py
+++ Visco.py
@@ -8,6 +8,15 @@ window.title("Banc de Viscosité")
 window.geometry("1080x720")
 window.minsize(1080,720)
 window.iconbitmap("uppa.ico")
+window.config(background="#304562")
+
+#Création d'un boutton
+Premier_boutton = Button(text="test du boutton", font=("Courrier", 15), fg='#B2BE12', bg='#233448')
+Premier_boutton.pack(side=LEFT,)
+
+#Création d'une image
+
+
 
 #Afficher la fenètre
 window.mainloop()
