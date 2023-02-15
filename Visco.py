a78a0309537ee250611987634b97352bc1cdb5c6
Nayati-Matrat <nayati.matrat@gmail.com>
2023-02-15 17:11:58 +0100

    Update Visco.py

--- Visco.py
+++ Visco.py
@@ -10,11 +10,18 @@ window.minsize(1080,720)
 window.iconbitmap("uppa.ico")
 window.config(background="#304562")
 
+#Création d'une image
+
+width = 100
+height = 100
+image = PhotoImage(file="logouppa.png")
+canvas = Canvas(window, width=width, height=height, bg='#304562', bd=0, highlightthickness=0)
+canvas.create_image(width/2, height/2, image=image)
+canvas.pack(anchor='nw')
+
 #Création d'un boutton
 Premier_boutton = Button(text="test du boutton", font=("Courrier", 15), fg='#B2BE12', bg='#233448')
-Premier_boutton.pack(side=LEFT,)
-
-#Création d'une image
+Premier_boutton.pack(anchor='nw')
 
 
 
