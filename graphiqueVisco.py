808a865f8dc3344af1e5bd8cf5dadfdde638a6df
Florent <florent.roch@hotmail.fr>
2023-02-18 18:55:16 +0100

    Update graphiqueVisco.py

--- graphiqueVisco.py
+++ graphiqueVisco.py
@@ -10,7 +10,22 @@ plt.plot(x, y)
 # Ajoutez des étiquettes pour les axes X et Y et un titre pour le graphique
 plt.xlabel("Axe X")
 plt.ylabel("Axe Y")
-plt.title("Mon Graphique")
+plt.title("Graphique Réel")
+
+# Affichez le graphique
+plt.show()
+
+# Créez des données pour le graphique
+w = [5, 6, 7, 8, 9]
+z = [2, 8, 18, 32, 50]
+
+# Créez un graphique avec les données
+plt.plot(w, z)
+
+# Ajoutez des étiquettes pour les axes X et Y et un titre pour le graphique
+plt.xlabel("Axe x")
+plt.ylabel("Axe Y")
+plt.title("Graphique Imaginaire")
 
 # Affichez le graphique
 plt.show()
