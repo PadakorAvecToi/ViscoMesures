94de66efb2ec4307872e5731aa2372ae2d32e38c
Nayati-Matrat <nayati.matrat@gmail.com>
2023-03-10 08:16:51 +0100

    Update Visco.py

--- Visco.py
+++ Visco.py
@@ -1,7 +1,30 @@
 from tkinter import *
+from tkinter import ttk
 
 #création de la fenètre
 window = Tk()
+
+#création d'un onglet
+notebook = ttk.Notebook(window)
+tab1 = Frame(notebook, bg="red")
+tab2 = Frame(notebook, bg="green")
+notebook.add(tab1, text="Onglet 1")
+notebook.add(tab2, text="Onglet 2")
+notebook.pack(expand=1, fill="both")
+
+#Ajouter des labels aux onglets
+Label(tab1, text="Contenu de l'onglet 1").pack(padx=10, pady=10)
+Label(tab2, text="Contenu de l'onglet 2").pack(padx=10, pady=10)
+
+# Créez un bouton pour cacher les onglets
+hide_button = Button(window, text="Cacher les onglets",
+                     command=lambda: notebook.hide())
+hide_button.pack(pady=10)
+
+# Créez un bouton pour afficher les onglets
+show_button = Button(window, text="Afficher les onglets",
+                     command=lambda: notebook.show(notebook.select()))
+show_button.pack(pady=10)
 
 #Modifications de la fenètre
 window.title("Banc de Viscosité")
@@ -11,7 +34,6 @@ window.iconbitmap("uppa.ico")
 window.config(background="#304562")
 
 #Création d'une image
-
 width = 2560
 height = 966
 image = PhotoImage(file="logouppa.png").subsample(5)
