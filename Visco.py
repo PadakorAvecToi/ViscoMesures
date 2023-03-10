b540d9559ae9e58b7a289e2375c352a36ec6c0a1
Nayati-Matrat <nayati.matrat@gmail.com>
2023-03-10 09:28:32 +0100

    Update Visco.py

--- Visco.py
+++ Visco.py
@@ -1,61 +1,38 @@
 from tkinter import *
-from tkinter import ttk
 
-#création de la fenètre
+# création de la fenêtre
 window = Tk()
 
-#création du gestionnaire d'onglets
-notebook = ttk.Notebook(window)
-
-# création des onglets
-tab1 = Frame(notebook)
-tab2 = Frame(notebook)
-tab3 = Frame(notebook)
-tab4 = Frame(notebook)
-tab5 = Frame(notebook)
-tab6 = Frame(notebook)
-tab7 = Frame(notebook)
-
-# ajout du texte dans les onglets
-Label(tab1, text="Contenu de l'onglet 1").pack(pady=20)
-Label(tab2, text="Contenu de l'onglet 2").pack(pady=20)
-Label(tab3, text="Contenu de l'onglet 3").pack(pady=20)
-Label(tab4, text="Contenu de l'onglet 4").pack(pady=20)
-Label(tab5, text="Contenu de l'onglet 5").pack(pady=20)
-Label(tab6, text="Contenu de l'onglet 6").pack(pady=20)
-Label(tab7, text="Contenu de l'onglet 7").pack(pady=20)
-
-# ajout des onglets au gestionnaire d'onglets
-notebook.add(tab1, text="Onglet 1")
-notebook.add(tab2, text="Onglet 2")
-notebook.add(tab3, text="Onglet 3")
-notebook.add(tab4, text="Onglet 4")
-notebook.add(tab5, text="Onglet 5")
-notebook.add(tab6, text="Onglet 6")
-notebook.add(tab7, text="Onglet 7")
-
-# placement des onglets dans une colonne
-notebook.grid(row=0, column=0, rowspan=7)
-
-#Modifications de la fenètre
-window.title("Banc de Viscosité")
-window.geometry("1080x720")
-window.minsize(1080,720)
-window.iconbitmap("uppa.ico")
-window.config(background="#304562")
-
-#Création d'une image
+# création d'une image
 width = 2560
 height = 966
 image = PhotoImage(file="logouppa.png").subsample(5)
 canvas = Canvas(window, width=width, height=height, bg='#304562', bd=0, highlightthickness=0)
 canvas.create_image(-200, -60, anchor=NW, image=image)
-canvas.pack()
 
-#Création d'un boutton
-Premier_boutton = Button(text="test du boutton", font=("Courrier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0)
-Premier_boutton.pack()
-Premier_boutton.place(x=10, y=100)
+# placement du canvas en-dessous des boutons
+canvas.grid(row=0, column=1, rowspan=7)
 
-#Afficher la fenètre
+# création des boutons
+def open_tab(tab_num):
+    # fonction pour ouvrir une nouvelle page en fonction du numéro de l'onglet
+    page = Toplevel(window)
+    Label(page, text=f"Contenu de l'onglet {tab_num}").pack(pady=20)
+
+Button(window, text="Onglet 1", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(1)).grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+Button(window, text="Onglet 2", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(2)).grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
+Button(window, text="Onglet 3", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(3)).grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
+Button(window, text="Onglet 4", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(4)).grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
+Button(window, text="Onglet 5", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(5)).grid(row=4, column=0, sticky="nsew", padx=10, pady=10)
+Button(window, text="Onglet 6", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(6)).grid(row=5, column=0, sticky="nsew", padx=10, pady=10)
+Button(window, text="Onglet 7", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(7)).grid(row=6, column=0, sticky="nsew", padx=10, pady=10)
+
+# modifications de la fenêtre
+window.title("Banc de Viscosité")
+window.geometry("1080x720")
+window.minsize(1080, 720)
+window.iconbitmap("uppa.ico")
+window.config(background="#304562")
+
+# afficher la fenêtre
 window.mainloop()
