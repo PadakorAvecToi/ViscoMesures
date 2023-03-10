14aa2b65b726c0a2ac964680f53a1b975e30d453
MatisMissana <m.missana.sn@gmail.com>
2023-03-10 08:36:45 +0100

    Update ConfigSynchrone.py

--- ConfigSynchrone.py
+++ ConfigSynchrone.py
@@ -1,20 +1,36 @@
-import serial
+import xml.etree.ElementTree as ET
 
-# Ouverture de la connexion série
-ser = serial.Serial('COM1', 9600, timeout=1)
+# Création d'un élément racine
+root = ET.Element("configurations")
 
-# Envoi des commandes de configuration
-ser.write(b'FREQM 1000\r\n') # Configure la fréquence de modulation à 1000 Hz
-ser.write(b'FREQREF 10000\r\n') # Configure la fréquence de référence à 10 kHz
-ser.write(b'FREQCUT 2000\r\n') # Configure la fréquence de coupure à 2 kHz
+# Ajout d'un élément pour chaque configuration
+config1 = ET.SubElement(root, "configuration")
+config1.set("nom", "Config 1")
 
-# Lecture de la réponse du détecteur synchrone
-response = ser.readline()
-print(response)
+freq_modulation = ET.SubElement(config1, "frequence_modulation")
+freq_modulation.text = "1000"
 
-# Fermeture de la connexion série
-ser.close()
+freq_reference = ET.SubElement(config1, "frequence_reference")
+freq_reference.text = "5000"
 
+freq_coupure = ET.SubElement(config1, "frequence_coupure")
+freq_coupure.text = "2000"
+
+config2 = ET.SubElement(root, "configuration")
+config2.set("nom", "Config 2")
+
+freq_modulation = ET.SubElement(config2, "frequence_modulation")
+freq_modulation.text = "2000"
+
+freq_reference = ET.SubElement(config2, "frequence_reference")
+freq_reference.text = "6000"
+
+freq_coupure = ET.SubElement(config2, "frequence_coupure")
+freq_coupure.text = "3000"
+
+# Écriture des configurations dans un fichier XML
+tree = ET.ElementTree(root)
+tree.write("configurations.xml")
 
 
 
