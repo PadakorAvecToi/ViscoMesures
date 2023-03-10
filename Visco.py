63fb3c249b855449a6c3b110b4d64ee40d58706b
Nayati-Matrat <nayati.matrat@gmail.com>
2023-03-10 14:19:51 +0100

    Update Visco.py

--- Visco.py
+++ Visco.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from  PIL import ImageTK, Image
+from PIL import ImageTk, Image
 
 # création de la fenêtre
 window = Tk()
@@ -9,63 +9,12 @@ window = Tk()
 framelogo=ttk.Frame(window)
 framelogo.place(x=800, y=1500)
 
-<<<<<<< HEAD
 #insertion du logo
-logo= ImageTK.PhotoImage(Image.open("logouppa.png"))
+logo= ImageTk.PhotoImage(Image.open("logouppa.png"))
 
 #création du label pour le logo
 labellogo = Label(window, image=logo)
 labellogo.pack()
-=======
-
-
-
-# création des boutons
-def open_tab(tab_num):
-    # fonction pour ouvrir une nouvelle page en fonction du numéro de l'onglet
-    page = Toplevel(window)
-    Label(page, text=f"Contenu de l'onglet {tab_num}").pack(pady=20)
-
-Button(window, text="Onglet 1", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, height= 1, width= 1, command=lambda: open_tab(1)).grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 2", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(2)).grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 3", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(3)).grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 4", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(4)).grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 5", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(5)).grid(row=4, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 6", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(6)).grid(row=5, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 7", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(7)).grid(row=6, column=0, sticky="nsew", padx=10, pady=10)
-
-
-
-
-
-
-
-
-
-# création des boutons
-def open_tab(tab_num):
-    # fonction pour ouvrir une nouvelle page en fonction du numéro de l'onglet
-    page = Toplevel(window)
-    Label(page, text=f"Contenu de l'onglet {tab_num}").pack(pady=20)
-
-Button(window, text="Onglet 1", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, height= 1, width= 1, command=lambda: open_tab(1)).grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 2", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(2)).grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 3", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(3)).grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 4", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(4)).grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 5", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(5)).grid(row=4, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 6", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(6)).grid(row=5, column=0, sticky="nsew", padx=10, pady=10)
-Button(window, text="Onglet 7", font=("Courier", 15), fg='#B2BE12', bg='#233448', bd=0, highlightthickness=0, command=lambda: open_tab(7)).grid(row=6, column=0, sticky="nsew", padx=10, pady=10)
-
-
-
-
-
-
-# placement du canvas en-dessous des boutons
-canvas.grid(row=0, column=1, rowspan=7)
->>>>>>> 3707901dcdaefb492820cadb511a43fbe0c1f50b
-
-
 
 # modifications de la fenêtre
 window.title("Banc de Viscosité")
