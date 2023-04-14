f2b1e4b2c7c1e8c31e05bb1caa8f76dffdee50d3
Grout Tom <tom.grout12@gmail.com>
2023-04-14 08:56:02 +0200

    Update mesureTemp.py

---
+++ mesureTemp.py
@@ -0,0 +1,46 @@
+#import nidaqmx
+#import time
+
+# Création d'une instance du périphérique d'acquisition de données
+#with nidaqmx.Task() as task:
+    # Configuration du canal d'entrée analogique
+    #task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+    
+    # Configuration de la mesure de température en utilisant une thermistance
+    #task.ai_channels.ai_term_cfg = nidaqmx.constants.TerminalConfiguration.DIFF
+    #task.ai_channels.add_ai_thrmcpl_chan(
+        #physical_channel="Dev1/ai1" ,
+        #name_to_assign_to_channel='',
+        #min_val=-10.0, 
+        #max_val=80.0, 
+        #units=nidaqmx.constants.TemperatureUnits.DEG_F, 
+        #thermocouple_type=nidaqmx.constants.ThermocoupleType.J
+       # )
+
+    # Lecture de la tension du canal d'entrée
+    #voltage = task.read()
+
+    # Conversion de la tension en température
+    #temperature = nidaqmx.constants.Thermocouple.convert_volts_to_temperature(voltage, nidaqmx.constants.ThermocoupleType.TYPE_J)
+
+    # Affichage de la température
+    #print("Température : {:.2f} °C".format(temperature))
+
+
+import nidaqmx
+import nidaqmx.constants
+import time
+
+# Création d'une instance du périphérique d'acquisition de données
+with nidaqmx.Task() as task:
+    # Configuration du canal d'entrée analogique
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+    
+    # Lecture de la tension du canal d'entrée
+    voltage = task.read()
+    temperature = nidaqmx.constants.UsageTypeAI.TEMPERATURE_THERMOCOUPLE
+
+    # Affichage de la tension
+    print("Voltage : {:.2f} V".format(voltage))
+    print (temperature )
+
