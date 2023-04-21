d6d52cda2629bdff11529811846d9ff4e285d4ea
Matrat Nayati <nayati.matrat@gmail.com>
2023-04-21 14:08:41 +0200

    Update Essai.py

--- Essai.py
+++ Essai.py
@@ -7,14 +7,9 @@ root = tk.Tk()
 
 # Définir la fonction pour ouvrir le fichier PowerPoint
 def open_powerpoint():
-    # Ouvrir une boîte de dialogue pour sélectionner le fichier
-    file_path = filedialog.askopenfilename(defaultextension=".pptx",
-                                           filetypes=[("test", "*.pptx")])
-    # Vérifier si un fichier a été sélectionné
-    if file_path:
-        # Ouvrir le fichier PowerPoint
-        os.startfile(file_path)
-
+    file_path = "test.pptx"
+    os.startfile(file_path)
+    
 # Créer le bouton
 button = tk.Button(root, text="Ouvrir PowerPoint", command=open_powerpoint)
 button.pack()
