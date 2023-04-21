8abc21125979a30d019f5f46ef8f96e0fe641b17
MatisMissana <m.missana.sn@gmail.com>
2023-04-21 14:59:19 +0200

    Update CarteD'acquisition.py

--- CarteD'acquisition.py
+++ CarteD'acquisition.py
@@ -1,17 +1,14 @@
 import serial.tools.list_ports
 
-# Obtenir la liste des ports série disponibles
+# Recherche de tous les ports disponibles
 ports = serial.tools.list_ports.comports()
 
-# Vérifier s'il y a des ports disponibles
-if len(ports) == 0:
-    print("Aucun port série disponible.")
-else:
-    print("Ports série disponibles :")
+# Affichage des informations sur les ports COM détectés
+for port in ports:
+    print(f"Port: {port.device} - Description: {port.description}")
 
-    # Afficher chaque port disponible
-    for port in ports:
-        print(f"- {port.device} : {port.description}")
+
+
 
 
 
@@ -24,7 +21,6 @@ else:
 
 import serial
 
-
 # Paramètres de communication série
 port = 'COM8'  # Remplacez par le nom de port série approprié (ex: '/dev/ttyUSB0' sur Linux)
 baudrate = 9600  # Vitesse de communication en bauds
@@ -32,26 +28,34 @@ bytesize = 8  # Taille des octets
 parity = 'N'  # Parité (N=aucune, E=pair, O=impair)
 stopbits = 1  # Nombre de bits de stop
 
+try:
+    # Ouvrir la connexion série
+    ser = serial.Serial(port=port, baudrate=baudrate, bytesize=bytesize, parity=parity, stopbits=stopbits)
 
-# Ouvrir la connexion série
-ser = serial.Serial(port=port, baudrate=baudrate, bytesize=bytesize, parity=parity, stopbits=stopbits)
+    # Envoyer la commande au détecteur synchrone
+    commande = "*IDN?\r"  # Commande à envoyer sans retour chariot à la fin
+    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
+
+    # Lire la réponse du détecteur synchrone
+    reponse = ""
+    while True:
+        caractere = ser.read().decode()
+        if caractere == "\r":
+            break
+        reponse += caractere
+    if reponse:
+        print(f"Réponse du détecteur synchrone : {reponse}")
+    else:
+        print("Aucune réponse du détecteur synchrone")
+
+except serial.SerialException as e:
+    print(f"Erreur de communication série : {e}")
+finally:
+    # Fermer la connexion série
+    if 'ser' in locals():
+        ser.close()
 
 
-# Envoyer la commande au détecteur synchrone
-commande = "*IDN?\r"  # Commande à envoyer avec un retour chariot à la fin
-ser.write(commande.encode())  # Envoyer la commande encodée en bytes
-
-
-# Lire la réponse du détecteur synchrone
-reponse = ser.readline().decode().strip()  # Lire la réponse et décoder en str
-if reponse:
-    print(f"Réponse du détecteur synchrone : {reponse}")
-else:
-    print("Aucune réponse du détecteur synchrone")
-
-
-# Fermer la connexion série
-ser.close()
 
 
 '''
