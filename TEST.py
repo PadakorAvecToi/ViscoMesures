10893ef48c655ec572d3c13e95572cef7f7ad6d5
MatisMissana <m.missana.sn@gmail.com>
2023-04-21 14:46:05 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,49 +1,36 @@
 import serial
-import time
-
-
-
 
 # Paramètres de communication série
 port = 'COM8'  # Remplacez par le nom de port série approprié (ex: '/dev/ttyUSB0' sur Linux)
 baudrate = 9600  # Vitesse de communication en bauds
+bytesize = 8  # Taille des octets
+parity = 'N'  # Parité (N=aucune, E=pair, O=impair)
+stopbits = 1  # Nombre de bits de stop
 
+try:
+    # Ouvrir la connexion série
+    ser = serial.Serial(port=port, baudrate=baudrate, bytesize=bytesize, parity=parity, stopbits=stopbits)
 
+    # Envoyer la commande au détecteur synchrone
+    commande = "*IDN?\r"  # Commande à envoyer sans retour chariot à la fin
+    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
 
-
-# Ouvrir la connexion série
-ser = serial.Serial(port, baudrate)
-
-
-
-
-# Envoyer la commande au détecteur synchrone
-commande = "*IDN?"  # Commande à envoyer
-ser.write(commande.encode())  # Envoyer la commande encodée en bytes
-
-
-
-
-# Attendre la réponse du détecteur synchrone (max 5 secondes)
-timeout = 5  # Temps d'attente maximal en secondes
-start_time = time.time()  # Instant initial
-while (time.time() - start_time) < timeout:
-    if ser.in_waiting > 0:
-        reponse = ser.readline().decode().strip()  # Lire la réponse et décoder en str
+    # Lire la réponse du détecteur synchrone
+    reponse = ""
+    while True:
+        caractere = ser.read().decode()
+        if caractere == "\r":
+            break
+        reponse += caractere
+    if reponse:
         print(f"Réponse du détecteur synchrone : {reponse}")
-        break
+    else:
+        print("Aucune réponse du détecteur synchrone")
 
-
-if 'reponse' not in locals():
-    print("Pas de réponse")
-
-
-
-
-# Fermer la connexion série
-ser.close()
-
-
-
-
+except serial.SerialException as e:
+    print(f"Erreur de communication série : {e}")
+finally:
+    # Fermer la connexion série
+    if 'ser' in locals():
+        ser.close()
 
