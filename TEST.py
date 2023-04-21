1df06c503fb5d4de0e510d48ef551fa2eca72983
MatisMissana <m.missana.sn@gmail.com>
2023-04-21 10:25:04 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,44 +1,47 @@
-
-
 import serial
+import time
+
+
+
 
 # Paramètres de communication série
-port = 'COM9'  # Remplacez par le nom de port série approprié (ex: '/dev/ttyUSB0' sur Linux)
+port = 'COM8'  # Remplacez par le nom de port série approprié (ex: '/dev/ttyUSB0' sur Linux)
 baudrate = 9600  # Vitesse de communication en bauds
+
+
+
 
 # Ouvrir la connexion série
 ser = serial.Serial(port, baudrate)
+
+
+
 
 # Envoyer la commande au détecteur synchrone
 commande = "*IDN?"  # Commande à envoyer
 ser.write(commande.encode())  # Envoyer la commande encodée en bytes
 
-# Lire la réponse du détecteur synchrone
-reponse = ser.readline().decode().strip()  # Lire la réponse et décoder en str
-print(f"Réponse du détecteur synchrone : {reponse}")
+
+
+
+# Attendre la réponse du détecteur synchrone (max 5 secondes)
+timeout = 5  # Temps d'attente maximal en secondes
+start_time = time.time()  # Instant initial
+while (time.time() - start_time) < timeout:
+    if ser.in_waiting > 0:
+        reponse = ser.readline().decode().strip()  # Lire la réponse et décoder en str
+        print(f"Réponse du détecteur synchrone : {reponse}")
+        break
+
+
+if 'reponse' not in locals():
+    print("Pas de réponse")
+
+
+
 
 # Fermer la connexion série
 ser.close()
-
-
-import serial.tools.list_ports
-
-# Recherche de tous les ports de communication disponibles
-ports = serial.tools.list_ports.comports()
-
-# Vérification de chaque port pour détecter le dispositif RS232
-for port in ports:
-    try:
-        # Récupération des informations sur le port
-        port_info = port.__dict__
-        # Affichage des informations du port détecté
-        print("Port: {}\nFabricant: {}\nDescription: {}\nNuméro de série: {}\n".format(port_info['device'], port_info['manufacturer'], port_info['description'], port_info.get('serial_number', 'N/A')))
-    except:
-        pass
-
-
-
-
 
 
 
