89105222f7eaa20dd5128b62cce72f9c7a5d91ce
Matrat Nayati <nayati.matrat@gmail.com>
2023-05-22 11:32:16 +0200

    Update Log.py

--- Log.py
+++ Log.py
@@ -14,7 +14,19 @@ def log_warning(message):
 
 def log_error(message, log_condition=True):
     if log_condition:
-        logging.error(message)
+        logger = logging.getLogger('my_logger')
+        logger.error(message)
+
+        # Vérifier le nombre de lignes dans le fichier de journalisation
+        logfile = 'C:\\Users\\letra\\Documents\\GitHub\\ViscoMesures\\Fichier_log.log'  # Remplacez par le chemin réel de votre fichier de journalisation
+        with open(logfile, 'r') as f:
+            lines = f.readlines()
+            num_lines = len(lines)
+
+        # Vider le fichier de journalisation si le nombre de lignes atteint 500
+        if num_lines >= 500:
+            with open(logfile, 'w'):
+                pass
 
 def log_critical(message):
     logging.critical(message)
