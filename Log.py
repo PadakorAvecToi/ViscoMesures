5f225b478e178f72be0b8b95841173c75a963beb
Matrat Nayati <nayati.matrat@gmail.com>
2023-05-22 11:35:37 +0200

    Update Log.py

--- Log.py
+++ Log.py
@@ -1,7 +1,7 @@
 import logging
 
 def setup_logger():
-    logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+    logging.basicConfig(filename='Fichier_log.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
 def log_debug(message):
     logging.debug(message)
