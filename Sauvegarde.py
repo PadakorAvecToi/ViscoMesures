6f4c1331c4d0bce36f62e0ad7a8dfa8177725945
MatisMissana <m.missana.sn@gmail.com>
2023-05-25 10:18:23 +0200

    Update Sauvegarde.py

--- Sauvegarde.py
+++ Sauvegarde.py
@@ -1,26 +1,66 @@
 import xml.etree.ElementTree as ET
+import tkinter as tk
+from tkinter import filedialog
+from FilVibrant import reponse
+from FilVibrant import nouvelle_tension
+from FilVibrant import nouvelle_frequence1
+from FilVibrant import nouveau_pas
+from FilVibrant import nouvelle_frequence2
+from FilVibrant import nouveau_sens
 
-def save_to_file(data, filename, file_format):
-    if file_format == 'xml':
-        root = ET.Element("viscosity_data")
-        for index, value in enumerate(data):
-            ET.SubElement(root, "measurement", index=str(index), value=str(value))
-        tree = ET.ElementTree(root)
-        tree.write(filename + '.xml')
-    elif file_format == 'txt':
-        with open(filename + '.txt', 'w') as f:
-            for index, value in enumerate(data):
-                f.write('Measurement ' + str(index) + ': ' + str(value) + '\n')
+def sauvegarder_configuration():
+    # Créer l'élément racine du fichier XML
+    root = ET.Element("configuration")
+
+
+    # Récupérer les valeurs de configuration définies précédemment
+    frequence_demarrage = nouvelle_frequence1  # Exemple de valeur pré-définie
+    frequence_arret = nouvelle_frequence2  # Exemple de valeur pré-définie
+    pas_frequence = nouveau_pas  # Exemple de valeur pré-définie
+    sens_balayage = nouveau_sens  # Exemple de valeur pré-définie
+
+
+    # Créer les éléments correspondant aux paramètres de configuration
+    freq_demarrage_elem = ET.SubElement(root, "frequence_demarrage")
+    freq_demarrage_elem.text = frequence_demarrage
+
+
+    freq_arret_elem = ET.SubElement(root, "frequence_arret")
+    freq_arret_elem.text = frequence_arret
+
+
+    pas_freq_elem = ET.SubElement(root, "pas_frequence")
+    pas_freq_elem.text = pas_frequence
+
+
+    sens_balayage_elem = ET.SubElement(root, "sens_balayage")
+    sens_balayage_elem.text = sens_balayage
+
+
+    # Créer l'arbre XML
+    tree = ET.ElementTree(root)
+
+
+    # Demander à l'utilisateur de choisir le nom et l'emplacement du fichier
+    root = tk.Tk()
+    root.withdraw()
+    fichier_config = filedialog.asksaveasfilename(defaultextension=".xml", filetypes=[("Fichier XML", "*.xml")])
+
+
+    # Enregistrer le fichier XML
+    if fichier_config:
+        tree.write(fichier_config)
+        print("Configuration sauvegardée avec succès dans le fichier", fichier_config)
     else:
-        print("Invalid file format. Please enter either 'xml' or 'txt'.")
+        print("Annulation de la sauvegarde de la configuration")
 
-# Example usage:
-data = [1.2, 2.1, 3.4, 4.5]
-filename = input("Enter a file name: ")
-directory = input("Enter a directory path to save the file: ")
-file_format = input("Enter a file format (xml or txt): ")
 
-save_to_file(data, directory + '/' + filename, file_format)
+
+
+# Appeler la fonction pour sauvegarder la configuration
+sauvegarder_configuration()
+
+
 
 
 
