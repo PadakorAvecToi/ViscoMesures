fd6310936e1a37183a2a5b4b22a38a8b480572f7
MatisMissana <m.missana.sn@gmail.com>
2023-06-01 09:30:07 +0200

    Update CarteDacquisition.py

--- CarteDacquisition.py
+++ CarteDacquisition.py
@@ -1,55 +1,26 @@
 import serial.tools.list_ports
-import time
 
+ports = serial.tools.list_ports.comports()
+com_list = []
+for p in ports:
+    com_list.append(p.device)
+    print(p.device)
 
-def connect_to_device(device_name):
-    # Récupérer la liste des ports série disponibles
-    ports = serial.tools.list_ports.comports()
-
-
-    # Parcourir chaque port
-    for port in ports:
+# Boucle "do while"
+port_found = False
+while not port_found:
+    for com_port in com_list:
         try:
-            # Ouvrir la connexion série
-            ser = serial.Serial(port.device, baudrate=9600, timeout=1)
-            time.sleep(1)  # Attendre un court délai pour permettre au détecteur de s'initialiser
-
-
-            # Envoyer la commande pour récupérer l'IDN
-            ser.write(b"*IDN?\r")
-            time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur de répondre
-
-
-            # Lire la réponse de l'appareil
+            ser = serial.Serial(com_port, timeout=1)
+            ser.write(b'*IDN?\r')
             response = ser.readline().decode().strip()
-
-
-            # Fermer la connexion série
+            if response:
+                print(f"Port COM trouvé : {com_port}")
+                
+                port_found = True
+                break
             ser.close()
-
-
-            # Vérifier si la réponse contient le nom de l'appareil recherché
-            if device_name in response:
-                return port.device  # Retourner le port s'il correspond
-        except (OSError, serial.SerialException):
-            # Ignorer les erreurs de port série non valides
+        except serial.SerialException:
             pass
-
-
-    return None  # Retourner None si aucun appareil n'est trouvé
-
-
-# Utilisation de la fonction pour se connecter automatiquement au détecteur
-device_name = "SRS SR850"
-com_port = connect_to_device(device_name)
-
-
-if com_port is not None:
-    print(f"Connexion réussie au détecteur {device_name} sur le port {com_port}.")
-else:
-    print(f"Échec de la connexion au détecteur {device_name}. Veuillez vérifier la connexion et les paramètres.")
-
-
-
 
 
