2ad19e3d72f22558f4e74bc592486432fb0e8850
MatisMissana <m.missana.sn@gmail.com>
2023-06-01 14:51:30 +0200

    Update FilVibrant.py

--- FilVibrant.py
+++ FilVibrant.py
@@ -15,15 +15,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(3.0)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
     
-    reponse = ""
+    reponse1 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere1 = ser.read().decode()
+        if caractere1 == "\r":
             break
-        reponse += caractere
+        reponse1 += caractere1
 
-    if reponse:
-        print(f"Fréquence actuelle du détecteur synchrone : {reponse}")
+    if reponse1:
+        print(f"Fréquence actuelle du détecteur synchrone : {reponse1}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -33,15 +33,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(3.0)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
     
-    reponse = ""
+    reponse2 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere2 = ser.read().decode()
+        if caractere2 == "\r":
             break
-        reponse += caractere
+        reponse2 += caractere2
 
-    if reponse:
-        print(f"Réponse du détecteur synchrone : {reponse}")
+    if reponse2:
+        print(f"Réponse du détecteur synchrone : {reponse2}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -85,15 +85,15 @@ try:
     commande = f"SLVL {nouvelle_tension:.2f}\r"  # Commande pour modifier la tension d'excitation avec un retour chariot à la fin
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
 
-    reponse = ""
+    reponse3 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere3 = ser.read().decode()
+        if caractere3 == "\r":
             break
-        reponse += caractere
+        reponse3 += caractere3
 
-    if reponse:
-        print(f"Réponse du détecteur synchrone : {reponse}")
+    if reponse3:
+        print(f"Réponse du détecteur synchrone : {reponse3}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -101,15 +101,15 @@ try:
     commande = "SLVL?\r"  # Commande pour récupérer la tension d'excitation avec un retour chariot à la fin
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
 
-    reponse = ""
+    reponse4 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere4 = ser.read().decode()
+        if caractere4 == "\r":
             break
-        reponse += caractere
+        reponse4 += caractere4
 
-    if reponse:
-        print(f"Nouvelle tension d'excitation du détecteur synchrone : {reponse}")
+    if reponse4:
+        print(f"Nouvelle tension d'excitation du détecteur synchrone : {reponse4}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -137,15 +137,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(3.0)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
     
-    reponse = ""
+    reponse4 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere4 = ser.read().decode()
+        if caractere4 == "\r":
             break
-        reponse += caractere
+        reponse4 += caractere4
 
-    if reponse:
-        print(f"Fréquence actuelle du détecteur synchrone : {reponse}")
+    if reponse4:
+        print(f"Fréquence de démmarage actuelle du détecteur synchrone : {reponse4}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -155,15 +155,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(3.0)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
     
-    reponse = ""
+    reponse5 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere5 = ser.read().decode()
+        if caractere5 == "\r":
             break
-        reponse += caractere
+        reponse5 += caractere5
 
-    if reponse:
-        print(f"Réponse du détecteur synchrone : {reponse}")
+    if reponse5:
+        print(f"Réponse du détecteur synchrone : {reponse5}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -191,15 +191,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(3.0)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
     
-    reponse = ""
+    reponse6 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere6 = ser.read().decode()
+        if caractere6 == "\r":
             break
-        reponse += caractere
+        reponse6 += caractere6
 
-    if reponse:
-        print(f"Fréquence actuelle du détecteur synchrone : {reponse}")
+    if reponse6:
+        print(f"Fréquence actuelle du détecteur synchrone : {reponse6}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -209,15 +209,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(3.0)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
     
-    reponse = ""
+    reponse7 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere7 = ser.read().decode()
+        if caractere7 == "\r":
             break
-        reponse += caractere
+        reponse7 += caractere7
 
-    if reponse:
-        print(f"Réponse du détecteur synchrone : {reponse}")
+    if reponse7:
+        print(f"Réponse du détecteur synchrone : {reponse7}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -235,8 +235,24 @@ import time
 
 
 # Paramètres de communication série
-port = 'COM4'  # Remplacez par le nom du port série approprié (par exemple, '/dev/ttyUSB0' sous Linux)
+port = 'COM9'  # Remplacez par le nom du port série approprié (par exemple, '/dev/ttyUSB0' sous Linux)
 baudrate = 9600  # Vitesse de communication en bauds
+Option_0 =0.0000625
+Option_1=0.000125
+Option_2 =0.00025
+Option_3=0.0005
+Option_4 =1.0
+Option_5 =2.0
+Option_6 =4.0
+Option_7 =8.0
+Option_8 =16.0
+Option_9 =32.0
+Option_10 =64.0
+Option_11 =128.0
+Option_12=256.0
+Option_13=512.0
+
+
 
 
 try:
@@ -250,36 +266,36 @@ try:
     time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
 
 
-    reponse = ""
+    reponse8 = ""
     while True:
-        caractere = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
-        if caractere == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+        caractere8 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
+        if caractere8 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
             break
-        reponse += caractere  # Ajouter le caractère à la réponse
+        reponse8 += caractere8  # Ajouter le caractère à la réponse
 
 
-    if reponse:
-        print(f"Pas de fréquence actuel du détecteur synchrone : {reponse}")
+    if reponse8:
+        print(f"Pas de fréquence actuel du détecteur synchrone : {reponse8}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
 
     # Modifier le pas de fréquence du détecteur synchrone
-    nouveau_pas = 0.01  # Remplacez par le pas de fréquence souhaité (nombre flottant)
-    commande = f"SRAT {nouveau_pas:.2f}\r"  # Commande pour modifier le pas de fréquence avec un retour chariot à la fin
+    nouveau_pas = Option_4  # Remplacez par le pas de fréquence souhaité (nombre flottant)
+    commande = f"SRAT {nouveau_pas:.9f}\r"  # Commande pour modifier le pas de fréquence avec un retour chariot à la fin
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
 
 
-    reponse = ""
+    reponse9 = ""
     while True:
-        caractere = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
-        if caractere == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+        caractere9 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
+        if caractere9 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
             break
-        reponse += caractere  # Ajouter le caractère à la réponse
+        reponse9 += caractere9  # Ajouter le caractère à la réponse
 
 
-    if reponse:
+    if reponse9:
         print(f"Nouveau pas de fréquence du détecteur synchrone : {nouveau_pas}")
     else:
         print("Aucune réponse du détecteur synchrone")
@@ -319,15 +335,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
 
-    reponse = ""
+    reponse10 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere10 = ser.read().decode()
+        if caractere10 == "\r":
             break
-        reponse += caractere
+        reponse10 += caractere10
 
-    if reponse:
-        print(f"Sens de balayage actuel du détecteur synchrone : {reponse}")
+    if reponse10:
+        print(f"Sens de balayage actuel du détecteur synchrone : {reponse10}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -344,15 +360,15 @@ try:
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
 
-    reponse = ""
+    reponse11 = ""
     while True:
-        caractere = ser.read().decode()
-        if caractere == "\r":
+        caractere11 = ser.read().decode()
+        if caractere11 == "\r":
             break
-        reponse += caractere
+        reponse11 += caractere11
 
-    if reponse:
-        print(f"Nouveau sens de balayage du détecteur synchrone : {reponse}")
+    if reponse11:
+        print(f"Nouveau sens de balayage du détecteur synchrone : {reponse11}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
@@ -365,9 +381,141 @@ finally:
         ser.close()
 
 
+import serial
+import time
 
+
+# Paramètres de communication série
+port = 'COM9'  # Remplacez par le nom du port série approprié (par exemple, '/dev/ttyUSB0' sous Linux)
+baudrate = 9600  # Vitesse de communication en bauds
+
+
+
+
+
+try:
+    # Ouvrir la connexion série
+    ser = serial.Serial(port, baudrate)
+
+
+    # Récupérer l'harmonique de détection actuel du détecteur synchrone
+    commande = "HARM?\r"  # Commande pour récupérer l'harmonique de détection avec un retour chariot à la fin
+    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
+    time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
+
+
+    reponse12 = ""
+    while True:
+        caractere12 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
+        if caractere12 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+            break
+        reponse12 += caractere12  # Ajouter le caractère à la réponse
+
+
+    if reponse12:
+        print(f"Harmonique de détecteur actuel du détecteur synchrone : {reponse12}")
+    else:
+        print("Aucune réponse du détecteur synchrone")
+
+
+    # Modifier l'harmonique de détection du détecteur synchrone
+    nouveau_harmondetec = 14  # Remplacez par l'harmonique de détection souhaité (nombre flottant)
+    commande = f"HARM {nouveau_harmondetec:.9f}\r"  # Commande pour modifier l'harmonique de détection avec un retour chariot à la fin
+    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
+    time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
+
+
+    reponse13 = ""
+    while True:
+        caractere13 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
+        if caractere13 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+            break
+        reponse13 += caractere13  # Ajouter le caractère à la réponse
+
+
+    if reponse13:
+        print(f"Nouvelle harmonique de détection du détecteur synchrone : {nouveau_harmondetec}")
+    else:
+        print("Aucune réponse du détecteur synchrone")
+
+
+except serial.SerialException as e:
+    print(f"Erreur de communication série : {e}")
+
+
+finally:
+    # Fermer la connexion série
+    if 'ser' in locals():
+        ser.close()
      
 
+
+import serial
+import time
+
+
+# Paramètres de communication série
+port = 'COM9'  # Remplacez par le nom du port série approprié (par exemple, '/dev/ttyUSB0' sous Linux)
+baudrate = 9600  # Vitesse de communication en bauds
+
+
+
+
+
+try:
+    # Ouvrir la connexion série
+    ser = serial.Serial(port, baudrate)
+
+
+    # Récupérer l'harmonique de détection actuel du détecteur synchrone
+    commande = "SLEN?\r"  # Commande pour récupérer la longeur de balayage avec un retour chariot à la fin
+    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
+    time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
+
+
+    reponse14 = ""
+    while True:
+        caractere14 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
+        if caractere14 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+            break
+        reponse14 += caractere14  # Ajouter le caractère à la réponse
+
+
+    if reponse14:
+        print(f"Longeur de balayage du détecteur synchrone : {reponse14}")
+    else:
+        print("Aucune réponse du détecteur synchrone")
+
+
+    # Modifier l'harmonique de détection du détecteur synchrone
+    nouvelle_longeur = 12  # Remplacez par la longeur de balayage en secondesouhaité 
+    commande = f"SLEN {nouvelle_longeur:.9f}\r"  # Commande pour modifier la longeur de balayage en seconde avec un retour chariot à la fin
+    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
+    time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
+
+
+    reponse15 = ""
+    while True:
+        caractere15 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
+        if caractere15 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+            break
+        reponse15 += caractere15  # Ajouter le caractère à la réponse
+
+
+    if reponse15:
+        print(f"Nouvelle longeur de balayage du détecteur synchrone : {nouvelle_longeur}")
+    else:
+        print("Aucune réponse du détecteur synchrone")
+
+
+except serial.SerialException as e:
+    print(f"Erreur de communication série : {e}")
+
+
+finally:
+    # Fermer la connexion série
+    if 'ser' in locals():
+        ser.close()
 
 
 
