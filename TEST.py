914f2ccaa375d662633091b6b93029fdf7e512b7
MatisMissana <m.missana.sn@gmail.com>
2023-06-01 15:40:23 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,67 +1,60 @@
 import serial
 import time
 
-
 # Paramètres de communication série
-port = 'COM9'  # Remplacez par le nom du port série approprié (par exemple, '/dev/ttyUSB0' sous Linux)
+port = 'COM9'  # Remplacez par le nom de port série approprié (ex: '/dev/ttyUSB0' sur Linux)
 baudrate = 9600  # Vitesse de communication en bauds
-
-
-
-
 
 try:
     # Ouvrir la connexion série
     ser = serial.Serial(port, baudrate)
 
-
-    # Récupérer l'harmonique de détection actuel du détecteur synchrone
-    commande = "SLEN?\r"  # Commande pour récupérer la longeur de balayage avec un retour chariot à la fin
+    # Récupérer le le mode de numérisation actuel du détecteur synchrone
+    commande = "SWPT?\r"  # Commande pour récupérer le type de balyage interne avec un retour chariot à la fin
     ser.write(commande.encode())  # Envoyer la commande encodée en bytes
     time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
 
-
-    reponse14 = ""
+    reponse22 = ""
     while True:
-        caractere14 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
-        if caractere14 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+        caractere22 = ser.read().decode()
+        if caractere22 == "\r":
             break
-        reponse14 += caractere14  # Ajouter le caractère à la réponse
+        reponse22 += caractere22
 
-
-    if reponse14:
-        print(f"Longeur de balayage du détecteur synchrone : {reponse14}")
+    if reponse22:
+        print(f"Mode de numérisation actuel du détecteur synchrone : {reponse22}")
     else:
         print("Aucune réponse du détecteur synchrone")
 
+    # Modifier le sens de balayage du détecteur synchrone
+    nouveau_typebalayage = input("Choisissez le type de balyage interne (0 pour linéaire , 1 pour logarithmique) : ")
+    if nouveau_typebalayage == "0":
+        commande5 = "SWPT 0\r"  # Commande pour définir le sens de balayage à montant avec un retour chariot à la fin
+    elif nouveau_typebalayage == "1":
+        commande5 = "SWPT 1\r"  # Commande pour définir le sens de balayage à descendant avec un retour chariot à la fin
+    else:
+        print("Choix invalide.")
+        exit()
 
-    # Modifier l'harmonique de détection du détecteur synchrone
-    nouvelle_longeur = 12  # Remplacez par la longeur de balayage en secondesouhaité 
-    commande = f"SLEN {nouvelle_longeur:.9f}\r"  # Commande pour modifier la longeur de balayage en seconde avec un retour chariot à la fin
-    ser.write(commande.encode())  # Envoyer la commande encodée en bytes
+    ser.write(commande5.encode())  # Envoyer la commande encodée en bytes
     time.sleep(0.1)  # Attendre un court délai pour permettre au détecteur synchrone de répondre
 
-
-    reponse15 = ""
+    reponse23 = ""
     while True:
-        caractere15 = ser.read().decode()  # Lire un caractère depuis le port série et le décoder en une chaîne de caractères
-        if caractere15 == "\r":  # Si le caractère est un retour chariot ("\r"), sortir de la boucle
+        caractere23 = ser.read().decode()
+        if caractere23 == "\r":
             break
-        reponse15 += caractere15  # Ajouter le caractère à la réponse
+        reponse23 += caractere23
 
-
-    if reponse15:
-        print(f"Nouvelle longeur de balayage du détecteur synchrone : {nouvelle_longeur}")
+    if reponse23:
+        print(f"Nouveau sens de balayage du détecteur synchrone : {reponse23}")
     else:
         print("Aucune réponse du détecteur synchrone")
-
 
 except serial.SerialException as e:
     print(f"Erreur de communication série : {e}")
-
 
 finally:
     # Fermer la connexion série
     if 'ser' in locals():
         ser.close()
-
