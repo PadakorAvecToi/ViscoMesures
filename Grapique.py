4f4ba3773362e8af3543a671b70c973e9dcca53b
Florent <florent.roch@hotmail.fr>
2023-06-14 20:23:55 +0200

    Update Grapique.py

--- Grapique.py
+++ Grapique.py
@@ -36,7 +36,7 @@ def test_graph1():
         # Ajoutez des étiquettes pour les axes X et Y et un titre pour chaque sous-graphique
         ax1.set_xlabel("Axe X", fontsize=12, color='yellow')
         ax1.set_ylabel("Axe Y", fontsize=12, color='yellow')
-        ax1.set_title("Partie Réele", fontsize=14, color='yellow')
+        ax1.set_title("Partie réelle", fontsize=14, color='yellow')
         ax2.set_xlabel("Axe X", fontsize=12, color='yellow')
         ax2.set_ylabel("Axe Y", fontsize=12, color='yellow')
         ax2.set_title("Partie Imaginaire", fontsize=14, color='yellow')
